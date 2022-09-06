00dcd36f7eaa82789ba2609ea32c82bc7cd06b05
saroven <ashah3562@gmail.com>
2022-09-06 09:57:16 +0600

    added all files

---
+++ js/vue_template.js
@@ -0,0 +1,9 @@
+var vm = new Vue({
+   el: "#vue_det",
+   data: {
+      firstname: "shah",
+      lastname: "Alam",
+      htmlcontent: "<div><h1>this is html content</h1></div>",
+      imgsrc: "img/img.jpeg"
+   }
+});
